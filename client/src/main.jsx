5f0bbd12377e1637013c4ae61001ee945f447ce0
dave <davedmj1725@gmail.com>
2024-08-10 23:41:57 +0530

    SCSS fix-II

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import "../App.scss";
+//import "../App.scss";
 import { BrowserRouter } from 'react-router-dom'; 
  
 
